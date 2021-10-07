bc831497090c5d9bf13a00b7d656f2ccfb0f2708
wissemayadi <wissem-ayadi@hotmail.com>
2021-10-07 16:32:08 +0100

    first remote

---
+++ src/components/Balance.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Balance = () => {
+    return (
+        <>
+           <h4>YOUR BALANCE</h4> 
+           <h1 id="balance">$0.00</h1>
+        </>
+    )
+}
+
+export default Balance
